3637921aec65659b32af3689626aa9fd97efb15e
ftuyama <ftuyama@gmail.com>
2017-08-02 13:29:20 -0300

    Some improves

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,13 @@
 module UsersHelper
   def fetch_github_image
     if @user.github
-      profile_json = RestClient.get('https://api.github.com/users/' + @user.github)
-      profile = JSON.parse(profile_json)
-      profile['avatar_url']
+      begin
+        profile_json = RestClient.get('https://api.github.com/users/' + @user.github)
+        profile = JSON.parse(profile_json)
+        profile['avatar_url']
+      rescue
+        nil
+      end
     end
   end
 end

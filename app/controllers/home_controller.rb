7a837d6bf26d259ceeba2deacbcd80857b3419d7
ftuyama <felipetuyama@gmail.com>
2019-05-22 20:05:06 -0300

    Update dependencies

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
-	skip_before_filter :verify_authenticity_token
+	skip_before_action :verify_authenticity_token
 
 	def index
 		# Gets user from URL parameter
